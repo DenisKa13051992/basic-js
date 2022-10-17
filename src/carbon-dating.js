006d8847f81085fb0b44fc9a378e1231bea34e2e
Dzianis Karnachenka <arcadievich13051992@gmail.com>
2022-10-17 22:51:10 +0300

    add: carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function dateSample(/* sampleActivity */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
+function dateSample(sampleActivity) {
+  let number = 0;
+  if (sampleActivity == undefined || sampleActivity == false || sampleActivity<0 || sampleActivity>15){ return false} else if (typeof sampleActivity !== "string") {return false} else if (isNaN(+sampleActivity)) {return false} else  {
+  number = (Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity)))/(0.693/HALF_LIFE_PERIOD)
+  return Math.ceil(number)}
 }
 
 module.exports = {
